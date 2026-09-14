e4961673a200ce3e217cc0b5faf069be8505306d
Ashish Kumar <3303ashish@gmail.com>
2026-09-14 18:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (71.60%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -10,31 +10,65 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head ==NULL || head->next==NULL || left == right) return head;
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* stop = dummy;
-        for(int i=1;i<left;++i){
-            stop = stop->next;
-        }
-        ListNode* curr = stop->next;
-        ListNode* prev = NULL;
-        ListNode* Next = NULL;
-        ListNode* leftNode = curr; // permanent in left 
-        while(left<right){
+    ListNode* reverseList(ListNode* head) {
+        //if(head == NULL || head->next == NULL ) return head;
+         ListNode* prev = NULL;
+         ListNode* curr = head;
+         ListNode* Next = head;
+         while(curr!=NULL){
             Next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = Next;
-            left++;
+            curr =  Next;
         }
-        Next = curr->next;
-        curr->next = prev;
-        stop->next = curr;
-        leftNode->next = Next;
+        return prev;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+         if(head ==NULL || head->next==NULL || left == right) return head;
+        // ListNode* dummy = new ListNode(0);
+        // dummy->next = head;
+        // ListNode* stop = dummy;
+        // for(int i=1;i<left;++i){
+        //     stop = stop->next;
+        // }
+        // ListNode* curr = stop->next;
+        // ListNode* prev = NULL;
+        // ListNode* Next = NULL;
+        // ListNode* leftNode = curr; // permanent in left 
+        // while(left<right){
+        //     Next = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = Next;
+        //     left++;
+        // }
+        // Next = curr->next;
+        // curr->next = prev;
+        // stop->next = curr;
+        // leftNode->next = Next;
         
-        return dummy->next;
-        
+        // return dummy->next;
+        int n = 1;
+        ListNode* temp = head;
+        ListNode* a = NULL;
+        ListNode* b = NULL;
+        ListNode* c = NULL;
+        ListNode* d = NULL;
+        while(temp){
+            if(n==left-1) a = temp;
+            if(n==left) b = temp;
+            if(n==right) c = temp;
+            if(n==right+1) d = temp;
+            temp = temp->next;
+            n++;
+        }
+        if(a!=NULL) a->next = NULL;
+        c->next = NULL;
+        c = reverseList(b);
+        if(a!=NULL) a->next = c;
+        b->next = d;
+        if(a!=NULL) return head; 
+        return c;
     }
 };
